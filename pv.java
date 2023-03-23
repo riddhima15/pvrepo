892fb6c59416c84f05a02beec543cd062ef6c700
riddhima15 <riddhimapradhan64@gmail.com>
2023-03-23 20:00:47 +0530

    first commit

---
+++ pv.java
@@ -0,0 +1,11 @@
+public class main
+{
+  public static void main(Strings[] args)
+    {
+           System.out.println("Hello world");
+int a=3, int b=5;
+int c = a+b;
+System.out.println(c);
+
+    }
+}
